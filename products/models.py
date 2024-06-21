b16a40c8c9caa0c5176692053698fc4b10855080
jlewis-89 <jlewis21689@live.com>
2024-06-21 13:38:53 +0100

    Added templates for cart, built up json data for models

--- products/models.py
+++ products/models.py
@@ -13,6 +13,21 @@ class Product(models.Model):
         return self.title
 
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Choice(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
